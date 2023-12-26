3361cebcc0209b8832703b86f8194cee5658b02b
davi.lemos <1davi.gabriel1@gmail.com>
2023-12-26 17:19:11 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using PuppeteerSharp;
+
+namespace TesteRelatorioPuppeter
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pdfGenerator = new PdfGenerator();
+            await pdfGenerator.GeneratePdfAsync();
+        }
+    }
+
+    internal class PdfGenerator
+    {
+        public async Task GeneratePdfAsync()
+        {
+            try
+            {
+                var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true, ExecutablePath = @"\Program Files\Google\Chrome\Application\chrome.exe" });
+                var page = await browser.NewPageAsync();
+
+                // Use um caminho relativo para o arquivo HTML
+                var htmlFilePath = @"file:///C:/Users/davi.lemos/Desktop/Projetos_C%23/TesteRelatorioPuppeter/bin/Debug/net6.0/SiscomPDFReportTemplateExample.html";
+                await page.GoToAsync(htmlFilePath);
+
+                // Gere o PDF
+                var pdfFilePath = Path.Combine(@"C:\Users\davi.lemos\Desktop\Projetos_C#\TesteRelatorioPuppeter\bin\Debug\net6.0", "PDFGerado.pdf");
+                await page.PdfAsync(pdfFilePath);
+
+                await browser.CloseAsync();
+                Console.WriteLine($"PDF gerado em: {pdfFilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao gerar o PDF: {ex.Message}");
+            }
+        }
+    }
+}
